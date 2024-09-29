a1900b19ccfbbd075471b5400d01b1171a323a09
pascal918 <pascal.kai98@outlook.com>
2024-09-29 23:40:55 +0200

    add dragmarker1

--- src/Carte.js
+++ src/Carte.js
@@ -1,6 +1,6 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback, useRef } from "react";
 
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Marker,Popup } from 'react-leaflet';
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 
@@ -99,6 +99,42 @@ function DisplayPosition({ map, setPolyline }) {
     );
 }
 
+
+function DraggableMarker() {
+    const [draggable, setDraggable] = useState(true)
+    const [position, setPosition] = useState(center)
+    const markerRef = useRef(null)
+    const eventHandlers = useMemo(
+      () => ({
+        dragend() {
+          const marker = markerRef.current
+          if (marker != null) {
+            setPosition(marker.getLatLng())
+          }
+        },
+      }),
+      [],
+    )
+    const toggleDraggable = useCallback(() => {
+      setDraggable((d) => !d)
+    }, [])
+    return (
+        <Marker
+          draggable={draggable}
+          eventHandlers={eventHandlers}
+          position={position}
+          ref={markerRef}>
+          <Popup minWidth={90}>
+            <span onClick={toggleDraggable}>
+              {draggable
+                ? 'Marker is draggable'
+                : 'Click here to make marker draggable'}
+            </span>
+          </Popup>
+        </Marker>
+      )
+    }
+
 function Carte() {
     const [map, setMap] = useState(null)
     const [polyline, setPolyline] = useState([
@@ -118,6 +154,7 @@ function Carte() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <DraggableMarker />
                 <Polyline pathOptions={limeOptions} positions={polyline} key={JSON.stringify(polyline)} />
             </MapContainer>
         ),
