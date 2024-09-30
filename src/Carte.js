5cca2b45d127bde271cf1a3e4d585a9fcfd67f49
pascal918 <pascal.kai98@outlook.com>
2024-09-30 09:19:22 +0200

    update polyline tag

--- src/Carte.js
+++ src/Carte.js
@@ -8,9 +8,13 @@ const center = [51.505, -0.09];
 const zoom = 13;
 const limeOptions = { color: 'lime' };
 
-function DisplayPosition({ map, setPolyline }) {
-    const [position, setPosition] = useState(() => map.getCenter())
-    const [routeList, setRouteList] = useState([{ latitude: "", longitude: "" }]);
+function Carte() {
+    const [map, setMap] = useState(null)
+    const [polyline, setPolyline] = useState([center ]);
+
+    //const [position, setPosition] = useState(() => map.getCenter())
+    const [routeList, setRouteList] = useState([{ latitude: center.at(0), longitude: center.at(1) }]);
+    const mapRef = useRef(null);
 
     const handleRouteChange = (e, index) => {
         const { name, value } = e.target;
@@ -22,7 +26,10 @@ function DisplayPosition({ map, setPolyline }) {
             const updatedPolyline = routeList.filter(item => item.latitude!== "" && item.longitude!== "" && item.longitude!== "-").map(item => [item.latitude, item.longitude]);
             setPolyline(updatedPolyline);
             console.log(updatedPolyline.at(-1), 'edit ways ',updatedPolyline)
-            map.flyTo(updatedPolyline.at(-1), zoom);
+            if (map.current) {
+                console.log("Edited, fly to last point")
+                map.current.flyTo(updatedPolyline.at(-1), zoom);
+            }
 
         }
     }
@@ -35,7 +42,10 @@ function DisplayPosition({ map, setPolyline }) {
         const updatedPolyline = list.filter(item => item.latitude!== "" && item.longitude!== "" && item.longitude!== "-").map(item => [item.latitude, item.longitude]);
         setPolyline(updatedPolyline);        
         console.log(updatedPolyline.at(-1), 'remove points ',updatedPolyline)
-        map.flyTo(updatedPolyline.at(-1), zoom);
+        if (map.current) {
+            console.log("removed, fly to last point")
+            map.current.flyTo(updatedPolyline.at(-1), zoom);
+        }
 
         
     };
@@ -45,62 +55,79 @@ function DisplayPosition({ map, setPolyline }) {
     };
 
     return (
-        <form className="App" autoComplete="off">
-            <div className="form-field">
-                <label htmlFor="route">Routes</label>
-                <p />
-                <label>latitude  longitude</label>
-                {routeList.map((singleRoute, index) => (
-                    <div key={index} className="routes">
-                        <div className="first-division">
-                            <input
-                                name="latitude"
-                                type='number'
-                                id="latitude"
-                                value={singleRoute.latitude}
-                                onChange={(e) => handleRouteChange(e, index)}
-                                required
-                            />
-                            {routeList.length - 1 === index && (
-                                <button
-                                    type="button"
-                                    onClick={handleRouteAdd}
-                                    className="add-btn"
-                                >
-                                    <span>+ Add new route</span>
-                                </button>
-                            )}
+        
+        <div>
+            <MapContainer 
+            center={center}
+            zoom={zoom}
+            scrollWheelZoom={false}
+            style={{ height: '600px', width: '800px', display: 'table-cell' }}
+            ref={setMap}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <DraggableMarker onDragEnd={() => map.current?.getCenter()} />
+            <Polyline pathOptions={limeOptions} positions={polyline} key={JSON.stringify(polyline)} />
+            </MapContainer>
+            <form className="App" autoComplete="off">
+                <div className="form-field">
+                    <label htmlFor="route">Routes</label>
+                    <p />
+                    <label>latitude  longitude</label>
+                    {routeList.map((singleRoute, index) => (
+                        <div key={index} className="routes">
+                            <div className="first-division">
+                                <input
+                                    name="latitude"
+                                    type='number'
+                                    id="latitude"
+                                    value={singleRoute.latitude}
+                                    onChange={(e) => handleRouteChange(e, index)}
+                                    required
+                                />
+                                {routeList.length - 1 === index && (
+                                    <button
+                                        type="button"
+                                        onClick={handleRouteAdd}
+                                        className="add-btn"
+                                    >
+                                        <span>+ Add new route</span>
+                                    </button>
+                                )}
+                            </div>
+                            <div className="second-division">
+                                <input
+                                    name="longitude"
+                                    type="number"
+                                    id="longitude"
+                                    value={singleRoute.longitude}
+                                    onChange={(e) => handleRouteChange(e, index)}
+                                    required
+                                />
+                            </div>
+                            <div className="third-division">
+                                {routeList.length!== 1 && (
+                                    <button
+                                        type="button"
+                                        onClick={() => handleRouteRemove(index)}
+                                        className="remove-btn"
+                                    >
+                                        <span>Del</span>
+                                    </button>
+                                )}
+                            </div>
                         </div>
-                        <div className="second-division">
-                            <input
-                                name="longitude"
-                                type="number"
-                                id="longitude"
-                                value={singleRoute.longitude}
-                                onChange={(e) => handleRouteChange(e, index)}
-                                required
-                            />
-                        </div>
-                        <div className="third-division">
-                            {routeList.length!== 1 && (
-                                <button
-                                    type="button"
-                                    onClick={() => handleRouteRemove(index)}
-                                    className="remove-btn"
-                                >
-                                    <span>Del</span>
-                                </button>
-                            )}
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </form>
+                    ))}
+                </div>
+            </form>
+        </div>
     );
 }
 
 
-function DraggableMarker() {
+function DraggableMarker({ onDragEnd }) {
     const [draggable, setDraggable] = useState(true)
     const [position, setPosition] = useState(center)
     const markerRef = useRef(null)
@@ -110,6 +137,9 @@ function DraggableMarker() {
           const marker = markerRef.current
           if (marker != null) {
             setPosition(marker.getLatLng())
+            if (onDragEnd) {
+                onDragEnd();
+            }
           }
         },
       }),
@@ -132,41 +162,6 @@ function DraggableMarker() {
             </span>
           </Popup>
         </Marker>
-      )
-    }
-
-function Carte() {
-    const [map, setMap] = useState(null)
-    const [polyline, setPolyline] = useState([
-        [51.505, -0.09],
-    ]);
-
-    const displayMap = useMemo(
-        () => (
-            <MapContainer
-                center={center}
-                zoom={zoom}
-                scrollWheelZoom={false}
-                ref={setMap}
-                style={{ height: '600px', width: '800px', display: 'table-cell' }}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <DraggableMarker />
-                <Polyline pathOptions={limeOptions} positions={polyline} key={JSON.stringify(polyline)} />
-            </MapContainer>
-        ),
-        [],
-    );
-
-    return (
-        <div>
-            {displayMap}
-            {map ? <DisplayPosition map={map} setPolyline={setPolyline} />: null}
-        </div>
-    )
+      );
 }
-
 export default Carte;
